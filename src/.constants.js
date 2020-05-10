f9b7c9b4c3c0f7015ca7dc4c78620d4c21cffe9e
adarshak103 <64827901+adarshak103@users.noreply.github.com>
2020-05-10 23:46:50 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,11 +1,11 @@
 module.exports = Object.freeze({
   TOKEN: '1223007308:AAE_TP590XVMta9rfimEgUv3nXr4Kq7sAjk', //required
   ARIA_SECRET: 'sdjvhdbwujgbnIKBHAIOabhfvioasdhb', //change as per needs.
-  ARIA_DOWNLOAD_LOCATION: '/root/downloads)', //-leave as it is
+  ARIA_DOWNLOAD_LOCATION: '/app/downloads)', //-leave as it is
   INDEX_DOMAIN: 'https://bot.gdrive2003.workers.dev/', // replace with your index domain. use https://github.com/ParveenBhadooOfficial/go-drive
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION -leave as it is
-  ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings -leave as it is
-  ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded -leave as it is
+  ARIA_FILTERED_DOMAINS: ['porn'], // Prevent downloading from URLs containing these substrings -leave as it is
+  ARIA_FILTERED_FILENAMES: ['porn'], // Files/top level directories with these substrings in the filename won't be downloaded -leave as it is
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well -leave as it is
   GDRIVE_PARENT_DIR_ID:  '16ec19CplQfdAPfDImpbaPsG8wUPqUaae', // for directorty eg. https://drive.google.com/drive/folders/1p7pHhSh2yboWePmo53DnVr3xkAYiojP9 use 1p7pHhSh2yboWePmo53DnVr3xkAYiojP9
   SUDO_USERS: [872733090],	// Telegram user IDs. These users can use the bot in any chat. required, get it using https://t.me/YourTGid_Bot
